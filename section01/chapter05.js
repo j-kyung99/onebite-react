6e630652d0e33a1d0c56b906569f7a4548e33f3f
j-kyung99 <apfwnls14@naver.com>
2024-09-24 21:22:25 +0900

    #1.5 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "갱";
+let myLocation = "대구";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다): 현재 어떤 값도 없다고 표현할 때 사용하는 값
+let empty = null;
+
+// 5. Undefined Type: 변수를 초기화하지 못했거나 존재하지 않는 값을 불려오려고 할 때 발생하는 값
+let none;
+console.log(none);
