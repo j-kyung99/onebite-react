ec0f6703fda203fd6272e381fc4bea7768d7a4dc
j-kyung99 <apfwnls14@naver.com>
2024-12-04 22:57:02 +0900

    #3.5 Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,8 +1,13 @@
 import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
 // import mul from "./math.js"; // default는 이름 마음대로 변경 가능
 // import { add, sub } from "./math.js";
 //const { add, sub } = require("./math");
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
